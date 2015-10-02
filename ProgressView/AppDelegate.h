7f368e9d99497dde9ba3bf22ac94e4a8d0d6c23a
jiadeyu <13521864217@163.com>
2015-10-02 12:10:48 +0800

    Initial Commit

---
+++ ProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgressView
+//
+//  Created by jiadeyu on 15/10/2.
+//  Copyright (c) 2015年 jiadeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressView
+//
+//  Created by jiadeyu on 15/10/2.
+//  Copyright (c) 2015年 jiadeyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
